eba5111433f47780693d8013a5bec9895ddc07b8
Surasak Prommarat <surasak@adapterdigital.com>
2020-01-15 12:32:15 +0700

    split

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const lineReader = require('line-reader');
 
 lineReader.eachLine('/home/pi/blue_hydra/blue_hydra_rssi.log', function(lines) {
-    var line = lines.split()
+    var line = lines.split(/(\s+)/).filter( function(e) { return e.trim().length > 0; } );
     console.log(line[1]);
 });
