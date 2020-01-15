c80aae351b882c91e86467664040143f6398be6c
Surasak Prommarat <surasak@adapterdigital.com>
2020-01-15 16:55:44 +0700

    python tailer

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from subprocess import PIPE, run
 for lines in tailer.follow(open("/home/pi/blue_hydra/blue_hydra_rssi.log")):
     line = lines.split()
     if(line[1] == 'CL'):
-        command = ['hcitool', 'name', line[2]]
-        result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True)
-        print(result.returncode, result.stdout, result.stderr)
+        print(line[2])
+        # command = ['hcitool', 'name', line[2]]
+        # result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+        # print(result.returncode, result.stdout, result.stderr)
