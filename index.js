0d5197025346d50a657bfeaa191b677cb7519db0
Surasak Prommarat <surasak@adapterdigital.com>
2020-01-15 15:56:28 +0700

    hcitool

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 var spawn = require('child_process').spawn
+const { exec } = require("child_process");
 const lineReader = require('line-reader')
 
-function run(mac) {
+function hciToolScan(mac) {
     return new Promise((resolve, reject) => {
         var command = spawn('hcitool', ['name ', mac])
         var result = ''
-        command.stdout.on('data', function(data) {
+        command.stdout.on('data', (data) => {
              result += data.toString()
         })
-        command.on('close', function(code) {
+        command.on('close', (code) => {
             resolve(result)
         })
-        command.on('error', function(err) { 
+        command.on('error', (err) => { 
             reject(err) 
         })
     })
@@ -24,7 +25,7 @@ lineReader.eachLine('/home/pi/blue_hydra/blue_hydra_rssi.log', function(lines) {
         var MAC = line[2]
         if(!checkList.includes(MAC)){
             console.log('checking:'+ MAC)
-            run(MAC)
+            hciToolScan(MAC)
             .then((response) => {
                 console.log('response:'+ response)
             })
