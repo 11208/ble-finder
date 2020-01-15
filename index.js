f9e5372f93c8b08d09868b62e85ff875739cfab8
Surasak Prommarat <surasak@adapterdigital.com>
2020-01-15 16:23:40 +0700

    hcitool

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const lineReader = require('line-reader')
 function hciToolScan(mac) {
     return new Promise((resolve, reject) => {
         var command = spawn('hcitool', ['name ', mac])
-        var result = mac+' NAME:'
+        var result = mac+' '
         command.stdout.on('data', (data) => {
              result += data.toString()
         })
@@ -19,19 +19,23 @@ function hciToolScan(mac) {
 }
 
 var checkList = []
-lineReader.eachLine('/home/pi/blue_hydra/blue_hydra_rssi.log', function(lines) {
-    var line = lines.split(/(\s+)/).filter( function(e) { return e.trim().length > 0; } )
-    if(line[1] == 'CL'){
-        var MAC = line[2]
-        if(!checkList.includes(MAC)){
-            hciToolScan(MAC)
-            .then((response) => {
-                console.log('MAC:'+ response)
-            })
-            .catch((error) => {
-                console.log(error)
-            })
-            checkList.push(MAC)
+while(1){
+    lineReader.eachLine('/home/pi/blue_hydra/blue_hydra_rssi.log', function(lines) {
+        var line = lines.split(/(\s+)/).filter( function(e) { return e.trim().length > 0; } )
+        if(line[1] == 'CL'){
+            var MAC = line[2]
+            if(!checkList.includes(MAC)){
+                hciToolScan(MAC)
+                .then((responses) => {
+                    console.log('Status:'+ responses)
+                    var response = responses.split(/(\s+)/).filter( function(e) { return e.trim().length > 0; } )
+                    checkList.filter(item => item !== response[0])
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+                checkList.push(MAC)
+            }
         }
-    }
-})
+    })
+}
