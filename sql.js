de7156074878ff020420c31d091e67a7503a6379
Surasak Prommarat <surasak@adapterdigital.com>
2020-01-16 09:45:01 +0700

    sql query

--- sql.js
+++ sql.js
@@ -4,22 +4,9 @@ var moment = require('moment');
 let db = new sqlite3.Database('/home/pi/blue_hydra/blue_hydra.db')
 let START_TIME = moment().subtract(1, "minutes").format('YYYY-MM-DD h:mm:ss')
 let STOP_TIME = moment().format('YYYY-MM-DD h:mm:ss')
-let sql = `
-    SELECT 
-        address, name, 
-        vendor, 
-        company, 
-        manufacturer, 
-        classic_mode AS classic, 
-        le_mode AS le, 
-        le_address_type, 
-        updated_at as last_seen, 
-        classic_major_class, 
-        classic_minor_class, 
-        classic_class
+let sql = `SELECT address, name, vendor, company, manufacturer, classic_mode AS classic, le_mode AS le, le_address_type, updated_at, classic_major_class, classic_minor_class, classic_class
     FROM blue_hydra_devices
-    WHERE last_seen BETWEEN ${START_TIME} AND ${STOP_TIME}
-`;
+    WHERE updated_at BETWEEN ${START_TIME} AND ${STOP_TIME}`;
  
 db.all(sql, [], (err, rows) => {
   if (err) {
