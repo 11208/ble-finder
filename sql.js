8d37e55fbd451f0ee2a3ecc7901ae17643f31236
Surasak Prommarat <surasak@adapterdigital.com>
2020-01-16 09:30:50 +0700

    sql query

--- sql.js
+++ sql.js
@@ -1,7 +1,9 @@
 const sqlite3 = require('sqlite3').verbose();
  
-let db = new sqlite3.Database('/home/pi/blue_hydra/blue_hydra.db');
- 
+var MS_PER_MINUTE = 60000;
+let db = new sqlite3.Database('/home/pi/blue_hydra/blue_hydra.db')
+let START_TIME = (date+"%s") - MS_PER_MINUTE
+let STOP_TIME = date +"%s"
 let sql = `
     SELECT 
         address, 
@@ -16,8 +18,9 @@ let sql = `
         classic_major_class, 
         classic_minor_class, 
         classic_class,
-        CAST(strftime('%s',updated_at) AS integer)
     FROM blue_hydra_devices
+    WHERE CAST(strftime('%s',updated_at) AS integer) 
+    BETWEEN CAST((${START_TIME} AS integer) AND CAST(${STOP_TIME} AS integer)
 `;
  
 db.all(sql, [], (err, rows) => {
