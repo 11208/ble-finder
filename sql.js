d66b0cc4547afd6015e541eca86600f93c5c1cdb
Surasak Prommarat <surasak@adapterdigital.com>
2020-01-16 09:20:53 +0700

    sql query

--- sql.js
+++ sql.js
@@ -3,7 +3,20 @@ const sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database('/home/pi/blue_hydra/blue_hydra.db');
  
 let sql = `
-    SELECT address, name, vendor, company, manufacturer, classic_mode AS classic, le_mode AS le, le_address_type, updated_at as last_seen, classic_major_class, classic_minor_class, classic_class 
+    SELECT 
+        address, 
+        name, 
+        vendor, 
+        company, 
+        manufacturer, 
+        classic_mode AS classic, 
+        le_mode AS le, 
+        le_address_type, 
+        updated_at as last_seen, 
+        classic_major_class, 
+        classic_minor_class, 
+        classic_class,
+        CAST(strftime('%s',updated_at) AS integer)
     FROM blue_hydra_devices
 `;
  
