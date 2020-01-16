3b68b774e5b4f67fe45034726899c0d0f3a50996
Surasak Prommarat <surasak@adapterdigital.com>
2020-01-16 09:59:52 +0700

    sql query

--- sql.js
+++ sql.js
@@ -2,8 +2,9 @@ const sqlite3 = require('sqlite3').verbose();
 var moment = require('moment');
 
 let db = new sqlite3.Database('/home/pi/blue_hydra/blue_hydra.db')
-let STOP_TIME = moment.utc().format('YYYY-MM-DD h:mm:ss')
-let START_TIME = moment.utc(STOP_TIME).local().subtract(1, "day").format('YYYY-MM-DD h:mm:ss')
+let CURRENT_TIME = moment.utc()
+let STOP_TIME = CURRENT_TIME.format('YYYY-MM-DD h:mm:ss')
+let START_TIME = moment.utc(CURRENT_TIME).local().subtract(1, "day").format('YYYY-MM-DD h:mm:ss')
 let sql = `SELECT address, name, vendor, company, manufacturer, classic_mode AS classic, le_mode AS le, le_address_type, updated_at, classic_major_class, classic_minor_class, classic_class
     FROM blue_hydra_devices
     WHERE updated_at BETWEEN '${START_TIME}' AND '${STOP_TIME}'`;
