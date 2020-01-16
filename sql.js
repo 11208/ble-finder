873e287afe5b7c5d9e0cde41dbcd786afc0d57a6
Surasak Prommarat <surasak@adapterdigital.com>
2020-01-16 10:28:18 +0700

    sql query

--- sql.js
+++ sql.js
@@ -5,13 +5,13 @@ let CURRENT_TIME = moment()
 let STOP_TIME = CURRENT_TIME.unix()
 let START_TIME = CURRENT_TIME.subtract(1, "minute").unix()
 
-let db = new sqlite3.Database('/home/pi/blue_hydra/blue_hydra.db')
 let sql = `SELECT address, name, vendor, company, manufacturer, classic_mode AS classic, le_mode AS le, le_address_type, updated_at, classic_major_class, classic_minor_class, classic_class
     FROM blue_hydra_devices
     WHERE CAST(strftime('%s',updated_at) AS integer)  
     BETWEEN ${START_TIME} AND ${STOP_TIME}`
 
 function fetchData(){
+  let db = new sqlite3.Database('/home/pi/blue_hydra/blue_hydra.db')
   db.all(sql, [], (err, rows) => {
     if (!err) {
       rows.forEach((row) => {
@@ -24,6 +24,7 @@ function fetchData(){
       console.log(err)
     }
   })
+  db.close()
   setTimeout(fetchData, 5000);
 }
 fetchData()
