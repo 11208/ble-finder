b11a01517d606b43041b1eb14050ff75b29c67fd
Surasak Prommarat <surasak@adapterdigital.com>
2020-01-16 10:37:28 +0700

    sql query

--- sql.js
+++ sql.js
@@ -4,11 +4,8 @@ var moment = require('moment')
 let CURRENT_TIME = moment()
 let STOP_TIME = CURRENT_TIME.unix()
 let START_TIME = CURRENT_TIME.subtract(1, "minute").unix()
-
 let sql = `SELECT address, name, vendor, company, manufacturer, classic_mode AS classic, le_mode AS le, le_address_type, updated_at, classic_major_class, classic_minor_class, classic_class
-    FROM blue_hydra_devices
-    WHERE CAST(strftime('%s',updated_at) AS integer)  
-    BETWEEN ${START_TIME} AND ${STOP_TIME}`
+    FROM blue_hydra_devices`
 
 function fetchData(){
   console.log('\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\')
