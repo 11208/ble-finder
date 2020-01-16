18bdd3a4002c224a23397b9ff5a320aec5599b8c
Surasak Prommarat <surasak@adapterdigital.com>
2020-01-16 09:48:57 +0700

    sql query

--- sql.js
+++ sql.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 var moment = require('moment');
 
 let db = new sqlite3.Database('/home/pi/blue_hydra/blue_hydra.db')
-let START_TIME = moment().subtract(1, "minutes").format('YYYY-MM-DD h:mm:ss')
+let START_TIME = moment().subtract(1, "day").format('YYYY-MM-DD h:mm:ss')
 let STOP_TIME = moment().format('YYYY-MM-DD h:mm:ss')
 let sql = `SELECT address, name, vendor, company, manufacturer, classic_mode AS classic, le_mode AS le, le_address_type, updated_at, classic_major_class, classic_minor_class, classic_class
     FROM blue_hydra_devices
