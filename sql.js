89c988dd899c1ab412ae4b2d320d40b7b3df13f3
Surasak Prommarat <surasak@adapterdigital.com>
2020-01-24 11:00:12 +0700

    1day

--- sql.js
+++ sql.js
@@ -2,11 +2,11 @@ const sqlite3 = require('sqlite3').verbose()
 var moment = require('moment')
 
 let CURRENT_TIME = moment()
-let STOP_TIME = CURRENT_TIME.format('YYYY-MM-DD h:mm:ss')
-let START_TIME = CURRENT_TIME.subtract(1, "day").format('YYYY-MM-DD h:mm:ss')
+let STOP_TIME = CURRENT_TIME.unix()
+let START_TIME = CURRENT_TIME.subtract(1, "day").unix()
 let sql = `SELECT address, name, vendor, company, manufacturer, classic_mode AS classic, le_mode AS le, le_address_type, updated_at, classic_major_class, classic_minor_class, classic_class
     FROM blue_hydra_devices
-    WHERE updated_at BETWEEN '${START_TIME}' AND '${STOP_TIME}'`;
+    WHERE CAST(strftime('%s',updated_at) AS integer) BETWEEN '${START_TIME}' AND '${STOP_TIME}'`;
 
 function fetchData(){
   console.log('\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\')
